cb5e11ce15a7f7af835585e6da40dafcf55b57d5
michael <mvogelsang@rocketmail.com>
2016-11-11 20:23:03 -0500

    fix defines

--- main.py
+++ main.py
@@ -12,48 +12,54 @@ import random
 import networkx as nx
 import matplotlib.pyplot as plt
 from Tkinter import *
-#
-# class Population:
-# 	def __init__(self, graph, population_size, crossover_func):
-# 		self.graph = graph
-# 		self.population_size = population_size
-# 		self.crossover_func = crossover_func
-#
-# 	# generate random initial population
-# 	def get_init_population(self):
-# 	# put code shit
-#
-# 	# select parent for breeding
-# 	def parent_select(self):
-# 	## put code shit
-#
-# 	# generate new population
-# 	def breed_new_generation(self):
-# 	## put code shit
-#
-# 	# sort population
-# 	def sort_pop(self):
-# 	## put code shit
-#
-# 	# find fittest member of population
-# 	def get_fittest(self):
-# 	## put code shit
-#
-# 	##def crossover(cut1, cut2):
-# 	## FILL IN LATER
-#
-# class Cut:
-# 	def __init__(self, cut, mutate_func):
-# 		self.cut = cut
-# 		self.fitness = self.get_fitness()
-# 		self.mutate_func = mutate_func
-#
-# 	# get fitness for stored cut
-# 	def get_fitness(self):
-# 	## put code shit
-#
-# 	##def mutate(self):
-# 	## FILL IN LATER
+
+class Population:
+	def __init__(self, graph, population_size, crossover_func):
+		self.graph = graph
+		self.population_size = population_size
+		self.crossover_func = crossover_func
+
+	# generate random initial population
+	def get_init_population(self):
+		pass
+	# put code shit
+
+	# select parent for breeding
+	def parent_select(self):
+		pass
+	## put code shit
+
+	# generate new population
+	def breed_new_generation(self):
+		pass
+	## put code shit
+
+	# sort population
+	def sort_pop(self):
+		pass
+	## put code shit
+
+	# find fittest member of population
+	def get_fittest(self):
+		pass
+	## put code shit
+
+	##def crossover(cut1, cut2):
+	## FILL IN LATER
+
+class Cut:
+	def __init__(self, cut, mutate_func):
+		self.cut = cut
+		self.fitness = self.get_fitness()
+		self.mutate_func = mutate_func
+
+	# get fitness for stored cut
+	def get_fitness(self):
+		pass
+	## put code shit
+
+	##def mutate(self):
+	## FILL IN LATER
 
 # function that returns a networkx graph fully initialized based on an input file
 def initializeGraph(filepath):
@@ -79,7 +85,7 @@ def initializeGraph(filepath):
 
 	cityfile.close()
 	return cityGraph
-	
+
 def main():
 	inputFile = sys.argv[1]
 	cityGraph = initializeGraph(inputFile)
